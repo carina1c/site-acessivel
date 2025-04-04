a63930a200a63b8a97a6ee6225c4f2cd37aba493
nina <144173604+carina1c@users.noreply.github.com>
2025-04-04 19:30:47 +0000

    .

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+   
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     const alternaContraste = this.getElementById('alterna-contraste')
 
-    botaoDeAcessibilidade.addEventListener('click', function (){
-    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-    });
-
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+    })
+    
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
     });
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
     });
     alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste')
     })
 });
 
-SrcollReveal().reveal('#inicio',{delay:500});
-SrcollReveal().reveal('#tropicalia',{delay:500});
-SrcollReveal().reveal('#galeria',{delay:500});
-SrcollReveal().reveal('#contato',{delay:500});
+ScrollReveal().reveal('#inicio',{delay:500});
+ScrollReveal().reveal('#tropicalia',{delay:500});
+ScrollReveal().reveal('#galeria',{delay:500});
+ScrollReveal().reveal('#contato',{delay:500});
